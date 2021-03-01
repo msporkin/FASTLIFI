4c75cfb73fb9e64da4c2573098537145df05d803
mandy45632 <77900555+mandy45632@users.noreply.github.com>
2021-03-01 10:35:38 -0600

    Add files via upload
    Mobile module updates - confirmed validated at test case 3.6 meters

--- Search.py
+++ Search.py
@@ -84,21 +84,61 @@ while ledLit == False:
         print('Calibration complete')
         
         #Calculate new offset after calibration and align
-        frame, cnt, approx = hex_find()
-        offsetAngles, center = hex_position(frame, cnt, approx)
+        #read frame
+        ret, frame = cap.read()
+        
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)   #converts BRG -> HSV image
+        mask = cv2.inRange(hsv, lb, ub)   #masks all colors other than ideal green
+        
+        # Converting image to a binary image 
+        _,threshold = cv2.threshold(mask, 3, 255, cv2.THRESH_BINARY)
+        #Set up detector to detect shapes with same colors
+        detector, _ = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        
+        # Searching through every region selected to find the required polygon. 
+        for cnt in detector : 
+            area = cv2.contourArea(cnt) 
+            # Shortlisting the regions based on area. 
+            if area > 120:  
+                approx = cv2.approxPolyDP(cnt, 0.03 * cv2.arcLength(cnt, True), True) 
+                # Checking if the no. of sides of the selected region is 7. 
+                if (len(approx) == 6):
+                    targetFound = True
+                    offsetAngles, center = hex_position(frame, cnt, approx)
         printpos(offsetAngles)
         Align(offsetAngles, statPi.ap1, statPi.an1, statPi.bp1, statPi.bn1, statPi.ap2,
               statPi.an2, statPi.bp2, statPi.bn2)
         
         #Calculate and show new offset to confirm alignment
         while (True):
-            frame, cnt, approx = hex_find()
-            offsetAngles, _ = hex_position(frame, cnt, approx)
+            #read frame
+            ret, frame = cap.read()
+            
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)   #converts BRG -> HSV image
+            mask = cv2.inRange(hsv, lb, ub)   #masks all colors other than ideal green
+            
+            # Converting image to a binary image 
+            _,threshold = cv2.threshold(mask, 3, 255, cv2.THRESH_BINARY)
+            #Set up detector to detect shapes with same colors
+            detector, _ = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            
+            # Searching through every region selected to find the required polygon. 
+            for cnt in detector : 
+                area = cv2.contourArea(cnt) 
+                # Shortlisting the regions based on area. 
+                if area > 120:  
+                    approx = cv2.approxPolyDP(cnt, 0.03 * cv2.arcLength(cnt, True), True) 
+                    # Checking if the no. of sides of the selected region is 7. 
+                    if (len(approx) == 6):
+                        targetFound = True
+                        offsetAngles, center = hex_position(frame, cnt, approx)
+                
+                        cv2.putText(frame, str(offsetAngles),(100,500),font,1,(0,0,0),2)
+            
             cv2.imshow('Target Aligned', cv2.resize(frame, (960,540)))
             #leave the loop if triggered
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-            
         
      #leave the loop if triggered
     if cv2.waitKey(1) & 0xFF == ord('q'):
